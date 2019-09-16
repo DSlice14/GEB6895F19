01d94b82862252880cedfd0b755df77a1e8ba4f4
DSlice14 <douglas@stigler.com>
2019-09-16 19:58:36 -0400

    assignment1questions

--- assignment_01/my_answers/example_06.R
+++ assignment_01/my_answers/example_06.R
@@ -13,14 +13,14 @@ the_five_numbers <- function(num_1, num_2, num_3, num_4, num_5)
 #Part B
 ## Find average of the 5 seperate numbers.
 
-avg <- function({the_five_numbers/5})
+avg <- function({sum(the_five_numbers)/5})
 
 ###############
 #Part C
 ## Find max and min of the 5 seperate numbers.
 
-max <- function(max{the_five_numbers/5})
-min <- function(min{the_five_numbers/5})
+max <- function(max{the_five_numbers})
+min <- function(min{the_five_numbers})
 
 ###############
 #Part D
