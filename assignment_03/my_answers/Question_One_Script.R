659ef4ebb8bb4be0c8993c7940fc71cdec98eba3
DSlice14 <douglas@stigler.com>
2019-10-08 22:19:38 -0400

    assignment 3

---
+++ assignment_03/my_answers/Question_One_Script.R
@@ -0,0 +1,188 @@
+##################################################
+# 
+# GEB 6895: Tools for Business Intelligence
+# 
+# Assignment 3, Question 1
+# Regression with Data from Spreadsheet
+# 
+# Douglas Stigler
+# 
+# Due October 9, 2019
+# 
+##################################################
+# 
+# Clear workspace.
+rm(list=ls(all=TRUE))
+
+# Set working directory.
+# wd_path <- '/path/to/your/folder' 
+wd_path <- 'C:/Users/dougl/Documents/GEB6895/My_Mirror/GEB6895F19-1/assignment_03' 
+
+setwd(wd_path)
+
+# Check folder:
+getwd()
+
+
+##################################################
+# Read the dataset and run regression
+##################################################
+
+# Read the newly saved dataset.
+life_data <- read.csv(file = 'life_exp.csv')
+
+# Inspect the data.
+summary(life_data)
+
+#View Data
+View(life_data)
+
+# Column Names
+colnames(life_data)
+
+#Histogram
+hist(life_data$lifeexpect)
+
+##################################################
+# Full Model
+##################################################
+
+#Model with all variables
+life_full_model <- lm(data = life_data, 
+                    formula = lifeexpect ~ medinc + uninsured + smoke + obesity + teenbirth + gunlaw + metro)
+
+#Summarize full model
+summary(life_full_model)
+
+##################################################
+# Removing variables from full model
+##################################################
+
+#Model with -1 variables
+life_a_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ uninsured + smoke + obesity + teenbirth + gunlaw + metro)
+#Summarize -1 model
+summary(life_a_model)
+
+####################
+
+#Model with -2 variables
+life_b_model <- lm(data = life_data, 
+                   formula = lifeexpect ~ smoke + obesity + teenbirth + gunlaw + metro)
+
+#Summarize -2 model
+summary(life_b_model)
+
+####################
+
+#Model with -3 variables
+life_c_model <- lm(data = life_data, 
+                   formula = lifeexpect ~ smoke + obesity + teenbirth + metro)
+
+#Summarize -3 model
+summary(life_c_model)
+
+####################
+
+#Model with -4 variables
+life_d_model <- lm(data = life_data, 
+                   formula = lifeexpect ~ smoke + obesity + teenbirth)
+
+#Summarize -4 model
+summary(life_d_model)
+
+##################################################
+# Creating Alternative Model
+##################################################
+
+#Alt Model with 1 variable
+life_alt1_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc)
+
+#Summarize 1 variable
+summary(life_alt1_model)
+
+####################
+
+#Alt 2
+life_alt2_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured)
+
+#Summarize 2 variable
+summary(life_alt2_model)
+
+#R^2 Stat is better, keeping variable
+
+####################
+
+#Alt 3
+life_alt3_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured + smoke)
+
+#Summarize 3 variable
+summary(life_alt3_model)
+
+#R^2 Stat is better, keeping variable
+
+#####################
+
+#Alt 4
+life_alt4_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured + smoke + obesity)
+
+#Summarize 4 variable
+summary(life_alt4_model)
+
+#R^2 Stat is better, keeping variable
+
+#####################
+
+#Alt 5
+life_alt5_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured + smoke + obesity + teenbirth)
+
+#Summarize 5 variable
+summary(life_alt5_model)
+
+#R^2 Stat is better, keeping variable
+
+#####################
+
+#Alt 6
+life_alt6_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured + smoke + obesity + teenbirth + gunlaw)
+
+#Summarize 6 variable
+summary(life_alt6_model)
+
+#R^2 Stat is better, keeping variable
+
+#####################
+
+#Alt 7
+life_alt7_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ medinc + uninsured + smoke + obesity + teenbirth + gunlaw + metro)
+
+#Summarize 7 variable
+summary(life_alt7_model)
+
+#R^2 Stat is better, keeping variable
+
+
+##################################################
+# Finding model to challange full model for most significant 
+##################################################
+
+#Alt significant
+#Came to these variables that produced significant t values within .05 after some trial and error
+
+life_alt8_model <- lm(data = life_data, 
+                      formula = lifeexpect ~ smoke + obesity + teenbirth)
+
+#Summarize significant variable
+summary(life_significant_model)
+
+
+##################################################
+# End
+##################################################
---
+++ assignment_03/my_answers/Question_Two_Script.R
@@ -0,0 +1,78 @@
+##################################################
+# 
+# GEB 6895: Tools for Business Intelligence
+# 
+# Assignment 3, Question 2
+# Regression with Data from Spreadsheet
+# 
+# Douglas Stigler
+# 
+# Due October 9, 2019
+# 
+##################################################
+# 
+# Clear workspace.
+rm(list=ls(all=TRUE))
+
+# Set working directory.
+# wd_path <- '/path/to/your/folder' 
+wd_path <- 'C:/Users/dougl/Documents/GEB6895/My_Mirror/GEB6895F19-1/assignment_03' 
+
+setwd(wd_path)
+
+# Check folder:
+getwd()
+
+
+##################################################
+# Read the dataset and run regression
+##################################################
+
+# Read the newly saved dataset.
+hospital_data <- read.csv(file = 'hospital_choice.csv')
+
+# Inspect the data.
+summary(hospital_data)
+
+#View Data
+View(hospital_data)
+
+# Column Names
+colnames(hospital_data)
+
+##################################################
+# Full Linear Model
+##################################################
+
+#Model with all variables
+life_linear_model <- lm(data = hospital_data, 
+                      formula = D ~ DISTANCE + INCOME + OLD)
+
+#Summarize full model
+summary(life_linear_model)
+
+##################################################
+# Full Logistic Model
+##################################################
+
+#Model with all variables
+life_log_model <- glm(data = hospital_data, 
+                      formula = D ~ DISTANCE + INCOME + OLD)
+
+#Summarize full model
+summary(life_log_model)
+
+##################################################
+# Logistic Model #2
+##################################################
+
+#Model with all variables
+life_log2_model <- glm(data = hospital_data, 
+                      formula = D ~ DISTANCE + INCOME + OLD + OLD*DISTANCE)
+
+#Summarize full model
+summary(life_log2_model)
+
+##################################################
+# End
+##################################################
